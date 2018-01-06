8087a856fd4445f6e525c8fbfb22576846b95c74
Brad Hambrice <hambrice@uab.edu>
2018-01-06 03:27:22 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -67,10 +67,7 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{:yukihiro_matsumoto => {
-   :known_for => "Ruby",
-   :languages => ["LISP", "C"]
- }
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -84,7 +81,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto]= {known_for:"Ruby", languages: ["LISP", "C"]
     
 end
 
